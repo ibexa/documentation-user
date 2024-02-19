4158a4c3a14572d9ad94419f10a25bac050ac0c5
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2024-02-19 08:21:07 +0100

    main.py: cards: get right version from abs_url

--- main.py
+++ main.py
@@ -48,6 +48,8 @@ def define_env(env):
         canonical = current_page.canonical_url
         url_parts = re.search("//([^/]+)/([^/]+)/([^/]+)/([^/]+)/([^/]+)/", canonical)
         (site, project, edition, language, version) = url_parts.groups()
+        url_parts = re.search("^/([^/]+)/([^/]+)/([^/]+)/([^/]+)/", absolute_url)
+        (project, edition, language, version) = url_parts.groups()
         version = force_version or version
 
         if isinstance(pages, str):
@@ -87,4 +89,4 @@ def define_env(env):
                     )
                 )
 
-        return """<div class="%s col-%s">%s</div>""" % (style, columns, "\n".join(cards))
+        return """<div class="%s col-%s">%s %s %s</div>""" % (style, columns, "\n".join(cards), current_page.abs_url, current_page.canonical_url)
