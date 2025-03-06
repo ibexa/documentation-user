ef43a9959f65c07062ce7e22236bdc98a65ceced
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2025-03-06 16:01:10 +0100

    instantsearch.js: Fix navigation
    - Fix first page link icon
    - hide "last page" link (this is not even the last page)

--- docs/js/instantsearch.js
+++ docs/js/instantsearch.js
@@ -142,14 +142,17 @@
             container: paginationContainer,
             padding: 2,
             templates: {
-                first: `<svg class="tile-icon" width="20" height="20">
-                    <use fill="var(--ibexa-dusk-black)" xlink:href="../images/ez-icons.svg#caret-expanded"></use>
+                first: `<svg class="tile-icon" width="16" height="16">
+                    <use fill="var(--ibexa-dusk-black)" xlink:href="../images/ez-icons.svg#caret-double-back"></use>
                 </svg>`,
                 previous: `<svg class="tile-icon" width="20" height="20">
                     <use fill="var(--ibexa-dusk-black)" xlink:href="../images/ez-icons.svg#caret-back"></use>
                 </svg>`,
                 next: `<svg class="tile-icon" width="20" height="20">
                     <use fill="var(--ibexa-dusk-black)" xlink:href="../images/ez-icons.svg#caret-next"></use>
+                </svg>`,
+                last: `<svg class="tile-icon" width="16" height="16">
+                    <use fill="var(--ibexa-dusk-black)" xlink:href="../images/ez-icons.svg#caret-double-next"></use>
                 </svg>`,
             },
         }),
