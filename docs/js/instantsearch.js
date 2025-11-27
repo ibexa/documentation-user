464038e44d6133e23e260983d011656be3eb37b8
Adrien Dupuis <61695653+adriendupuis@users.noreply.github.com>
2025-11-27 14:23:39 +0100

    Fix search page (#380)
    * instantsearch.js: Fix instantsearch credentials and index name
    * instantsearch.js: Fix initialUiState
    * instantsearch.js: Fix version parsing and default version
    * instantsearch.css: Fix spacing in result item CSS

--- docs/js/instantsearch.js
+++ docs/js/instantsearch.js
@@ -4,18 +4,18 @@
     const parsed_search_query = decodeURI(search_query.replaceAll('+', ' '));
     const search_page = (match = doc.location.search.match(/p=(\d*?)(&|$)/)) ? match[1] : 1;
     const parsed_search_page = parseInt(search_page);
-    let version = doc.location.pathname.split('/')[2];
+    let version = doc.location.pathname.split('/')[4];
     if (!/^\d+\.\d+$/.test(version) && version !== 'latest') {
-        version = 'master';
+        version = 'latest';
     }
     const hitsContainer = '#hits';
     const statsContainer = '#stats';
     const paginationContainer = '#pagination';
     const search = instantsearch({
-        indexName: 'ezplatform',
-        searchClient: algoliasearch('2DNYOU6YJZ', '21ce3e522455e18e7ee16cf7d66edb4b'),
+        indexName: 'ezplatform_userguide',
+        searchClient: algoliasearch('WLX2XJZTRM', '40ac288790bce56f11de4f1fb8e8ded4'),
         initialUiState: {
-            ezplatform: {
+            ezplatform_userguide: {
                 query: parsed_search_query,
                 refinementList: { version: [version] },
                 page: parsed_search_page,
