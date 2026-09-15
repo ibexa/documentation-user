bd65509d026a28f96deb0819a5ad2e6e6a4c7e2a
Marek Nocoń <mnocon@users.noreply.github.com>
2026-09-15 10:23:36 +0200

    Fixed error page for SaaS

--- docs/js/error-page.js
+++ docs/js/error-page.js
@@ -1,26 +1,33 @@
 (function (global, doc) {
     const errorPageContainer = doc.querySelector('.page-not-found');
+    const specialVersions = ['latest', 'saas'];
+    const latestVersionNumber = '5.0';
 
     if (!errorPageContainer) {
         return;
     }
 
     // Find the correct version
-    let currentVersion = '6.0';
+    let branchName = latestVersionNumber;
     const branchNameRegexp = /\/en\/([a-z0-9-_.]*)\//g.exec(document.location.href);
 
     if (branchNameRegexp !== null && branchNameRegexp.hasOwnProperty(1) && branchNameRegexp[1].length) {
-        currentVersion = branchNameRegexp[1];
+        branchName = branchNameRegexp[1];
     }
+
+    if (!/^\d+\.\d+$/.test(branchName) && !specialVersions.includes(branchName)) {
+        branchName = latestVersionNumber;
+    }
+
 
     // Replace all links in the TOC and in the error page content
     doc.querySelectorAll('.md-sidebar--primary .md-nav__item a, .page-not-found a').forEach(link => {
-        link.href = link.href.replace(/\/en\/([a-z0-9-_.]*)\//, `/en/${currentVersion}/`);
+        link.href = link.href.replace(/\/en\/([a-z0-9-_.]*)\//, `/en/${branchName}/`);
     });
 
     // Use the 404 URL path in initial search query
     const searchLink = document.querySelector('#search-link');
-    const suffix = window.location.href.split('/en/' + currentVersion + '/')[1];
+    const suffix = window.location.href.split('/en/' + branchName + '/')[1];
 
     const searchQuery = suffix.replaceAll('/', ' ').replaceAll('_', ' ');
     searchLink.href = searchLink.href.replace('?sq=', '?sq=' + encodeURIComponent(searchQuery));
